36eff16d6af3f1e3d9110ce263219813688f3bad
Barış Ekmekçi <barisekmekci@stu.aydin.edu.tr>
2024-04-16 17:42:06 +0300

    animals added and rows and columns are numbere d

--- src/TraderVoyagesUI/src/sections/Game/GameMap.tsx
+++ src/TraderVoyagesUI/src/sections/Game/GameMap.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React from "react";
+import KoordinatTablosu from "./KoordinatTablosu";
 
 const Game: React.FC = () => {
-    return (
-        <>
-        <div className='main'>
-         <div className="heroDiv">
+  return (
+    <>
+      <span>
+        <img
+          className="elephant"
+          src="/src/assets/images/elephant-real.png"
+        ></img>
+      </span>
+      <span>
+        <img className="horse" src="/src/assets/images/horse-real.png"></img>
+      </span>
+
+
+      
+      <div className="main">
+        <KoordinatTablosu />
+        <div className="heroDiv">
           {[...Array(20).keys()].map((index) => (
             <div className="innerDiv" key={index}>
-              <span className='VerticalNumber pt-1' id='X'></span>
+              <span className="VerticalNumber pt-1" id="X"></span>
             </div>
           ))}
         </div>
         <div className="heroDiv2">
           {[...Array(40).keys()].map((index) => (
-            <div className="outerDiv" id='Y' key={index}></div>
+            <div className="outerDiv" id="Y" key={index}></div>
           ))}
-         </div>
         </div>
-      </>
-        );
-    }
+      </div>
+    </>
+  );
+};
 
 export default Game;
